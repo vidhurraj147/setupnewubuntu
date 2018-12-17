febd7776fc022baec0393059e7e8e87184b50dbb
vidhurraj147 <37847430+vidhurraj147@users.noreply.github.com>
2018-12-16 23:16:03 -0500

    Adding JAVA VARIABLES
    Adding JAVA VARIABLES

--- initialUbuntuSetup.sh
+++ initialUbuntuSetup.sh
@@ -3,12 +3,23 @@ echo 'MAKE SURE YOU ARE LOGGED IN AS ROOT USER'
 sudo apt-get update -y
 #supo apt-get install yum -y
 sudo apt-get update && apt-get upgrade -y
+echo '--------INSTALLING JAVA START---------------'
 sudo apt-get install openjdk-8-jdk -y
+echo '--------INSTALLING JAVA DONE ---------------'
+echo '--------ADDING VARIABLES etc profile---------------'
 sudo echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /etc/profile
 sudo echo 'JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/' >> /etc/profile
 sudo echo 'PATH=$PATH:$HOME/bin:$JAVA_HOME/bin:$JAVA_HOME/jre/bin' >> /etc/profile
 sudo echo 'export JAVA_HOME' >> /etc/profile
 sudo echo 'export JRE_HOME' >> /etc/profile
 sudo echo 'export PATH' >> /etc/profile
+echo '--------ADDING VARIABLES BASHRC---------------'
+sudo echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /etc/profile
+sudo echo 'JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/' >> /etc/profile
+sudo echo 'PATH=$PATH:$HOME/bin:$JAVA_HOME/bin:$JAVA_HOME/jre/bin' >> /etc/profile
+sudo echo 'export JAVA_HOME' >> /etc/profile
+sudo echo 'export JRE_HOME' >> /etc/profile
+sudo echo 'export PATH' >> /etc/profile
+
 echo 'SCRIPT ABOUT TO APPLY NEW PATHS ABOUT TO EXIT LOGIN BACK'
 exit
