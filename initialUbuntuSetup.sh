6e771d66fd53dd2a3539c38bfac9e5a6d51d1939
vidhurraj147 <vidhurraj147@gmail.com>
2018-12-16 19:23:43 -0500

    ADDED JAVA PATHS

--- initialUbuntuSetup.sh
+++ initialUbuntuSetup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+echo 'MAKE SURE YOU ARE LOGGED IN AS ROOT USER'
 sudo apt-get update -y
 #supo apt-get install yum -y
 sudo apt-get update && apt-get upgrade -y
 sudo apt -get install openjdk-8-jdk -y
+sudo echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /etc/profile
+sudo echo 'JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/' >> /etc/profile
+sudo echo 'PATH=$PATH:$HOME/bin:$JAVA_HOME/bin:$JAVA_HOME/jre/bin' >> /etc/profile
+sudo echo 'export JAVA_HOME' >> /etc/profile
+sudo echo 'export JRE_HOME' >> /etc/profile
+sudo echo 'export PATH' >> /etc/profile
+echo 'SCRIPT ABOUT TO APPLY NEW PATHS ABOUT TO EXIT LOGIN BACK'
+exit
