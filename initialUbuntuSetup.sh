1eb3f881c95ef3323e0ec811a1df8bf4c5fae5f3
vidhurraj147 <37847430+vidhurraj147@users.noreply.github.com>
2018-12-16 22:50:43 -0500

    Update initialUbuntuSetup.sh

--- initialUbuntuSetup.sh
+++ initialUbuntuSetup.sh
@@ -3,7 +3,7 @@ echo 'MAKE SURE YOU ARE LOGGED IN AS ROOT USER'
 sudo apt-get update -y
 #supo apt-get install yum -y
 sudo apt-get update && apt-get upgrade -y
-sudo apt -get install openjdk-8-jdk -y
+sudo apt-get install openjdk-8-jdk -y
 sudo echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /etc/profile
 sudo echo 'JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/' >> /etc/profile
 sudo echo 'PATH=$PATH:$HOME/bin:$JAVA_HOME/bin:$JAVA_HOME/jre/bin' >> /etc/profile
