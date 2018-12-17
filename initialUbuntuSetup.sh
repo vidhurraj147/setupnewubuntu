f5641c5e9f838ff18e34fd196c0d79436959fb25
vidhurraj147 <37847430+vidhurraj147@users.noreply.github.com>
2018-12-16 23:17:19 -0500

    Adding variables bashrc
    Adding variables bashrc

--- initialUbuntuSetup.sh
+++ initialUbuntuSetup.sh
@@ -14,12 +14,12 @@ sudo echo 'export JAVA_HOME' >> /etc/profile
 sudo echo 'export JRE_HOME' >> /etc/profile
 sudo echo 'export PATH' >> /etc/profile
 echo '--------ADDING VARIABLES BASHRC---------------'
-sudo echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /etc/profile
-sudo echo 'JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/' >> /etc/profile
-sudo echo 'PATH=$PATH:$HOME/bin:$JAVA_HOME/bin:$JAVA_HOME/jre/bin' >> /etc/profile
-sudo echo 'export JAVA_HOME' >> /etc/profile
-sudo echo 'export JRE_HOME' >> /etc/profile
-sudo echo 'export PATH' >> /etc/profile
+sudo echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
+sudo echo 'JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/' >> ~/.bashrc
+sudo echo 'PATH=$PATH:$HOME/bin:$JAVA_HOME/bin:$JAVA_HOME/jre/bin' >> ~/.bashrc
+sudo echo 'export JAVA_HOME' >> ~/.bashrc
+sudo echo 'export JRE_HOME' >> ~/.bashrc
+sudo echo 'export PATH' >> ~/.bashrc
 
 echo 'SCRIPT ABOUT TO APPLY NEW PATHS ABOUT TO EXIT LOGIN BACK'
 exit
